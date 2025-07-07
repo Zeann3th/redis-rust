55219e0ea0ed2771258a841d85814f1cc2c1ded9
zeann3th <longhienly112@gmail.com>
2025-07-07 14:21:20 +0700

    feat(#RG2): PONG

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                match stream.write_all(b"+PONG\r\n") {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Failed to write to stream: {}", e);
+                        continue;
+                    }
+                };
             }
             Err(e) => {
                 println!("error: {}", e);
