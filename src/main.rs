a9db6e0753a891e84dcc5efb015c3735485e1a6f
zeann3th <longhienly112@gmail.com>
2025-07-07 14:35:29 +0700

    feat(#WY1): PING

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-#![allow(unused_imports)]
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -7,13 +9,47 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                match stream.write_all(b"+PONG\r\n") {
+                let mut buf = String::new();
+
+                match stream.read_to_string(&mut buf) {
                     Ok(_) => {}
                     Err(e) => {
-                        println!("Failed to write to stream: {}", e);
+                        println!("Failed to read from stream: {}", e);
                         continue;
                     }
                 };
+
+                let mut buf = buf.split(" ");
+
+                match buf.next() {
+                    Some(command) if command.to_uppercase() == "PING" => match buf.next() {
+                        Some(arg) => {
+                            match stream.write_all(format!("{}\r\n", arg).as_bytes()) {
+                                Ok(_) => {}
+                                Err(e) => {
+                                    println!("Failed to write to stream: {}", e);
+                                    continue;
+                                }
+                            };
+                        }
+                        None => {
+                            match stream.write_all(b"PONG\r\n") {
+                                Ok(_) => {}
+                                Err(e) => {
+                                    println!("Failed to write to stream: {}", e);
+                                    continue;
+                                }
+                            };
+                        }
+                    },
+                    _ => {
+                        println!(
+                            "Received unknown command: {}",
+                            buf.collect::<Vec<&str>>().join(" ")
+                        );
+                        continue;
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
